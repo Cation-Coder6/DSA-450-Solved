d63546094edad48978118d669a96b171a4d4a439
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-02-13 03:39:22 +0530

    Coin Change Problem

--- Dynamic Programming/2.Coin-Change-Ways.cpp
+++ Dynamic Programming/2.Coin-Change-Ways.cpp
@@ -1,21 +1,21 @@
-long long int count(int s[], int m, int n)
+int change(int amount, vector<int> &coins)
 {
-
-    long long int t[m + 1][n + 1];
-
-    for (int i = 0; i < m + 1; i++)
+    int length = coins.size();
+    int t[length + 1][amount + 1];
+    for (int i = 0; i < length + 1; i++)
     {
-        for (int j = 0; j < n + 1; j++)
+        for (int j = 0; j < amount + 1; j++)
         {
             if (i == 0 && j != 0)
-                t[i][j] = 0;
+                t[i][j] = 0; // if we have amount>0 but no coins i.e length = 0 thus false
             else if (j == 0)
-                t[i][j] = 1;
-            else if (s[i - 1] <= j)
-                t[i][j] = t[i - 1][j] + t[i][j - s[i - 1]];
+                t[i][j] = 1; // if amount = 0 then empty set will work as well thus true
+            else if (j >= coins[i - 1])
+                t[i][j] = t[i - 1][j] + t[i][j - coins[i - 1]]; // if current coin is lesser than amount we can select it and come back to it or not select it at all(because it is unbounded cause no of coins is unlimited).
+
             else
-                t[i][j] = t[i - 1][j];
+                t[i][j] = t[i - 1][j]; // the current coin is larger than the amount and thus we dont consider it
         }
     }
-    return t[m][n];
+    return t[length][amount];
 }
